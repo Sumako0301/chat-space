7c73d0e98d938b8550d817b91ab134bd0f5992de
Sumako0301 <sumako0301@gmail.com>
2019-11-22 20:17:15 +0900

    incremental-Serch create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  # before_action :set_group, only: [:edit, :update]
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -18,6 +18,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
